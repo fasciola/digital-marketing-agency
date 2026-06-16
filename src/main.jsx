c773f35e84dc3593e7f12c41483ecb5e38f86979
fasciola <faisalmousa86@gmail.com>
2026-06-16 17:27:16 +0400

    Use new dark agency design entry

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import './hero-curve.css';
-import './image-integration.css';
-import './hero-layout-fix.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
